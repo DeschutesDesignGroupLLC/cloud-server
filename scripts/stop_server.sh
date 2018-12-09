007db326119902b425d76a5750e1b61ae6313e86
EC2 Default User <ec2-user@ip-10-0-2-223.us-west-2.compute.internal>
2018-12-09 03:01:22 +0000

    Update stop server scripts

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Check if gedit is running
+# -x flag only match processes whose name (or command line if -f is
+# specified) exactly match the pattern. 
+
+echo "Killing node if it is still running..."
+
+if pgrep -x "node" > /dev/null
+then
+    killall node
+    echo "Node is no longer running..."
+else
+    echo "Node was not running..."
+fi
