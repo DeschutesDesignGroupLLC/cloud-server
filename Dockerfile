70c5ff27bc638e7a1b90bfff571720f5b840d91b
JonErickson <jonerickson@me.com>
2018-12-16 15:16:12 -0800

    Updated AWS ELB deployment

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10.14
+FROM node:latest
 
 # File Author / Maintainer
 MAINTAINER Deschutes Design Group LLC
@@ -9,7 +9,7 @@ RUN npm install pm2 -g && pm2 update
 # Create our working directory and copy our source to it
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-COPY parse-server .
+COPY . .
 
 # Install our NPM dependencies
 RUN npm install
@@ -18,4 +18,4 @@ RUN npm install
 EXPOSE 1337
 
 # Start the server
-CMD ["pm2-runtime", "start" ,"process.yml", "--env", "production"]
+CMD ["pm2-runtime", "start" ,"process.yml" ]
--- app.js
+++ app.js
@@ -1,37 +1,62 @@
 // Require express
 var express = require('express');
-var ParseServer = require('app').ParseServer;
-var app = express();
+var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
+
+// Set our db URI
+var databaseUri = process.env.PARSE_SERVER_DATABASE_URI || process.env.MONGODB_URI;
+if (!databaseUri) {
+    console.log('DATABASE_URI not specified, falling back to localhost.');
+}
 
 // Start our parse server using enviroment variables for our config settings
 var api = new ParseServer({
-    databaseURI: process.env.PARSE_SERVER_DATABASE_URI,
-    cloud: process.env.PARSE_SERVER_CLOUD,
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.PARSE_SERVER_CLOUD || __dirname + '/cloud/cloud.js',
     appId: process.env.PARSE_SERVER_APPLICATION_ID,
     masterKey: process.env.PARSE_SERVER_MASTER_KEY,
     fileKey: process.env.PARSE_SERVER_FILE_KEY,
-    serverURL: process.env.PARSE_SERVER_URL
+    serverURL: process.env.PARSE_SERVER_URL || 'http://localhost:1337/parse'
 });
+
+// Start our express instance
+var app = express();
+
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-app.use('/parse', api);
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
 
-// Server the server up on port set in enviroment variables
-var port = process.env.PARSE_SERVER_PORT;
-app.listen(port, function() {
-    console.log('parse-server running on port ' + port);
+// Provide a path for health checks
+app.get('/status', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/status.html'));
 });
 
-//Parse Dashboard
+// Set the port the app will be listening on
+var port = process.env.PORT || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('Parse server running on port ' + port + '.');
+});
+
+// Dashboard options
+var options = {
+    allowInsecureHTTP: true,
+    cookieSessionSecret: "z0DP7aQ37Wv4gyqXkbtL"
+};
+
+// Parse Dashboard
 var ParseDashboard = require('parse-dashboard');
-var dashboard = new ParseDashboard({
+var dashboard = new ParseDashboard( {
     apps: [
         {
             appName: "PERSCOM Cloud Integration",
             appId: process.env.PARSE_SERVER_APPLICATION_ID,
             masterKey: process.env.PARSE_SERVER_MASTER_KEY,
             fileKey: process.env.PARSE_SERVER_FILE_KEY,
-            production: process.env.PARSE_SERVER_PRODUCTION,
+            production: process.env.PARSE_SERVER_PRODUCTION || false,
             serverURL: process.env.PARSE_SERVER_URL,
 	        iconName: "deschutesdesigngroup.jpg"
         }
@@ -42,20 +67,16 @@ var dashboard = new ParseDashboard({
             pass: "$2y$12$MQIVdiepPF098NaGF3aM7uz5il7NqEbVqNQa.ZsJzqIRRAslBXnIO"
         }
     ],
-    iconsFolder: "images",
+    iconsFolder: "public/images",
     useEncryptedPasswords: true,
     trustProxy: true,
-    cookieSessionSecret: "z0DP7aQ37Wv4gyqXkbtL"
-});
+}, options );
 
-// Allows insecure connections because SSL is being terminated at AWS ELB
-var allowInsecureHTTP = true;
-
-// Serve the Parse Dashboard on the /parsedashboard URL prefix
+// Serve the Parse Dashboard on the / URL prefix
 app.use('/', dashboard);
 
 // Server the dashboard up on the env var port
-var portdash = process.env.PARSE_DASHBOARD_PORT;
+var portdash = process.env.PARSE_DASHBOARD_PORT || 4040;
 app.listen(portdash, function() {
-    console.log('parse-dashboard running on port ' + portdash);
+    console.log('Parse dashboard running on port ' + portdash);
 });
